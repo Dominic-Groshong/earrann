4c012efc36ed2248e84fda6537725ac788bafe2c
Dominic Groshong <groshond@gmail.com>
2020-06-30 22:37:29 -0700

    refactor as class

--- scrape.py
+++ scrape.py
@@ -6,71 +6,72 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Perameters
-site_url = 'https://www.valleybrokers.com/762281'  # MLS sample to scrape
+class MLS():
 
+    base_url = 'https://www.valleybrokers.com/'  # MLS sample to scrape
 
-# Helper functions
-def request_site(site_url):
-    """Send a request and parse it through beautiful soup, returning a soup object
+    def __init__(self, mls_number):
+        """
+        Takes in MLS number adds it to url.
 
-    Args:
-        URL (string): Input url of webpage you want to scrape.
-    """
-    page = requests.get(site_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+        Args:
+            mls_number (int): MLS number of house to scrape.
+        """
+        self.url = self.base_url + str(mls_number)
 
-    return soup
+    def get_soup(self):
+        page = requests.get(self.url)
+        self.soup = BeautifulSoup(page.content, 'html.parser')
 
+    def search_soup(self):
+        """
+        What data do we want to get from soup?
+        ---------------------------------------
+        images
+        type of home
+        style (1 story, 2 story, etc.)
+        year built
+        lot acerage
+        directions
+        inclusions
+        tax year
+        taxes
+        square footage
+        acreage
+        garage
+        water
+        zoning
+        listing agency
+        """
 
-def search_soup():
-    """
-    What data do we want to get from soup?
-    ---------------------------------------
-    images
-    type of home
-    style (1 story, 2 story, etc.)
-    year built
-    lot acerage
-    directions
-    inclusions
-    tax year
-    taxes
-    square footage
-    acreage
-    garage
-    water
-    zoning
-    listing agency
-    """
+        self.get_soup()
 
-    soup = request_site(site_url)
+        data = {
+            'MLS': soup.find(text=re.compile('^MLS')),
+            'Address': {
+                'Street': soup.find(text=re.compile('^Address')),
+                'City': soup.find(text=re.compile('^City')),
+                'County': soup.find(text=re.compile('^County')),
+                'State': soup.find(text=re.compile('^State')),
+                'ZIP': soup.find(text=re.compile('^State')),
+            },
+            'Bedrooms': soup.find(text=re.compile('^Bedrooms')),
+            'Bathrooms': soup.find(text=re.compile('^Bathrooms')),
+            'Price': soup.find(text=re.compile('^Price')),
+            'Comments': soup.find(text=re.compile('^Comments')),
+        }
 
-    data = {
-        'MLS': soup.find(text=re.compile('^MLS')),
-        'Address': {
-            'Street': soup.find(text=re.compile('^Address')),
-            'City': soup.find(text=re.compile('^City')),
-            'County': soup.find(text=re.compile('^County')),
-            'State': soup.find(text=re.compile('^State')),
-            'ZIP': soup.find(text=re.compile('^State')),
-        },
-        'Bedrooms': soup.find(text=re.compile('^Bedrooms')),
-        'Bathrooms': soup.find(text=re.compile('^Bathrooms')),
-        'Price': soup.find(text=re.compile('^Price')),
-        'Comments': soup.find(text=re.compile('^Comments')),
-    }
+        #img_urls = []
+        # img_urls.append(site_url + soup.find(id="showhome_mainphoto").get('src'))
 
-    #img_urls = []
-    # img_urls.append(site_url + soup.find(id="showhome_mainphoto").get('src'))
+        # for image in soup.findAll(text=re.compile('(?<=photourl = \').*?(?=\';)')):
+        #     image.find(string=re.compile('(?<=photourl = \').*?(?=\';)'))
+        #     print(image)
+        #     # img_urls.append(site_url + image.get('src'))
 
-    # for image in soup.findAll(text=re.compile('(?<=photourl = \').*?(?=\';)')):
-    #     image.find(string=re.compile('(?<=photourl = \').*?(?=\';)'))
-    #     print(image)
-    #     # img_urls.append(site_url + image.get('src'))
-
-    print(data)
+        print(data)
 
 
 # Run Program
-search_soup()
+sample = MLS(762281)
+sample.get_soup()
